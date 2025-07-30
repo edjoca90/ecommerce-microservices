7db0d74dd6b7b8b9bb15baf3b09c84df21b621fc
JohanCardenas <edjoca90@gmail.com>
2025-07-30 12:47:04 -0500

    ajustes test producto

--- product-service/src/test/java/com/ecommerce/product_service/controller/ProductControllerTest.java
+++ product-service/src/test/java/com/ecommerce/product_service/controller/ProductControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.ecommerce.product_service.model.Product;
 import com.ecommerce.product_service.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+@ActiveProfiles("test")
 @WebMvcTest(ProductController.class)
 class ProductControllerTest {
 
